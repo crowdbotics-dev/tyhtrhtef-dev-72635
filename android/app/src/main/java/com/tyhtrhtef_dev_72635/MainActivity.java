0ac26aa08f2c77762718be023494774826048c5a
Crowdbotics <engineering@crowdbotics.com>
2023-03-28 08:04:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tyhtrhtef_dev_72635/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tyhtrhtef_dev_72635;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tyhtrhtef_dev_72635";
+  }
+}
